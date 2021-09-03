ee71d91ef618b3d38e43b827e6747c69d6619f33
Sarah Amiri <sarah.amiri28@gmail.com>
2021-09-03 15:21:28 +0430

    Add user to admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.admin import register
+from django.contrib.auth import get_user_model
 
 from apps.users.models import Profile, SupportMember
+
+
+@register(get_user_model())
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_supporter', 'is_active', )
 
 
 @register(Profile)
