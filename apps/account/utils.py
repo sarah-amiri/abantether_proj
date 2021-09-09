a8c036e1219cae419d383e962ba80181f7570800
Sarah Amiri <sarah.amiri28@gmail.com>
2021-09-09 23:54:40 +0430

    Check accounts are active

--- apps/account/utils.py
+++ apps/account/utils.py
@@ -4,23 +4,31 @@ from apps.account.names import initial_account_name
 
 def get_accounts_by_name(source_account_name=None, destination_account_name=None):
     if source_account_name and destination_account_name:
-        source_account = Account.objects(name=source_account_name).first()
-        destination_account = Account.objects(name=destination_account_name).first()
+        source_account = Account.objects(
+            name=source_account_name, status='Active').first()
+        destination_account = Account.objects(
+            name=destination_account_name, status='Active').first()
         return source_account, destination_account
 
     if source_account_name:
         try:
-            source_account = Account.objects(name=source_account_name)[0]
+            source_account = Account.objects(
+                name=source_account_name, status='Active')[0]
             currency = source_account.currency
-            destination_account = Account.objects(name=initial_account_name.format(currency))[0]
+            destination_account = Account.objects(
+                name=initial_account_name.format(currency), status='Active'
+            )[0]
         except (IndexError, AttributeError):
             source_account, destination_account = None, None
 
     else:
         try:
-            destination_account = Account.objects(name=destination_account_name)[0]
+            destination_account = Account.objects(
+                name=destination_account_name, status='Active')[0]
             currency = destination_account.currency
-            source_account = Account.objects(name=initial_account_name.format(currency))[0]
+            source_account = Account.objects(
+                name=initial_account_name.format(currency), status='Active'
+            )[0]
         except (IndexError, AttributeError):
             source_account, destination_account = None, None
 
