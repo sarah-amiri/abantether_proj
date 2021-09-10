c388e9a5298fc727269b9ad8bfd826ba949c682d
Sarah Amiri <sarah.amiri28@gmail.com>
2021-09-10 12:13:35 +0430

    Add factories for currency

--- apps/currency/tests/factories.py
+++ apps/currency/tests/factories.py
@@ -4,8 +4,20 @@ import string
 from apps.currency.models import Currency
 
 from factory.django import DjangoModelFactory
-from faker import Faker
+from factory import Faker
+
+
+def get_random_string(length=1, include_digits=True):
+    strings = string.ascii_uppercase
+    if include_digits:
+        strings += string.digits
+    return ''.join(random.choices(strings, k=length))
 
 
 class CurrencyFactory(DjangoModelFactory):
-    pass
+    code = get_random_string(3, include_digits=False)
+    name= Faker("name")
+
+    class Meta:
+        model = Currency
+        django_get_or_create = ['code']
